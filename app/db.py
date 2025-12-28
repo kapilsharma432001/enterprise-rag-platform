01a8dcd21acf5a68a6b8da75c938ad3862e2d333
Kapil Sharma <kapilsharma@Kapils-MacBook-Air.local>
2025-12-28 12:50:10 +0530

    env example file and app directory with secure database connection code

---
+++ app/db.py
@@ -0,0 +1,42 @@
+import asyncpg
+from contextlib import asynccontextmanager
+from fastapi import Request, HTTPException
+from app.config import settings
+
+class Database:
+    def __init__(self):
+        self._pool = None
+    
+    async def connect(self):
+        self.pool = await asyncpg.create_pool(dsn = settings.DATABASE_URL)
+        print("Database pool created.")
+    
+    async def close(self):
+        await self.pool.close()
+        print("Database pool closed.")
+
+db = Database()
+
+# security dependency
+# This function will be used as a dependency in FastAPI routes to get a database connection -> will be used in every API route
+# It ensures that NO query runs without a tenant ID
+async def get_db_connection(request: Request):
+    # simulate getting tenant_id from request headers (in real world, JWT token)
+    tenant_id = request.headers.get("X-Tenant-ID")
+    if not tenant_id:
+        raise HTTPException(status_code=400, detail="Tenant ID is required")
+    
+    # aquire connection from pool
+    async with db.pool.acquire() as conn:
+        transaction = conn.transaction()
+        await transaction.start()
+        try:
+            # Enforce security context
+            await conn.execute(f'SET app.current_tenant = {tenant_id};')
+
+            yield conn
+            await transaction.commit()
+        except Exception as e:
+            await transaction.rollback()
+            raise e
+
